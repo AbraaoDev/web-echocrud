6532becf394e2b549a805282c7e041d642a10777
AbraaoDev <abraaohomualdo@gmail.com>
2025-06-18 16:20:20 -0300

    feat: add custom font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
+  modules: ["shadcn-nuxt", "@nuxtjs/google-fonts"],
+  googleFonts: {
+    families: {
+      "Titillium Web": [400, 700],
+    },
+    display: "swap",
+  },
   css: ["~/assets/css/tailwind.css"],
-
   vite: {
     plugins: [tailwindcss()],
   },
-
-  modules: ["shadcn-nuxt"],
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || "/api/v1",
